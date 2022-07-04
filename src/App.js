cc1be4e3acde725dcdf0d642adfafe8c8ffd36de
Tmaclea6 <tmaclea6@unb.ca>
2022-07-04 18:05:59 -0300

    First partio commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{ useEffect, useState, useRef} from "react";
+import { Map, Marker } from 'pigeon-maps'
+
 
 function App() {
+  const [center, setCenter] = useState([45.92698182886522, -66.62735907832183])
+  const [zoom, setZoom] = useState(11)
+  const [hue, setHue] = useState(0)
+  const color = `hsl(${hue % 360}deg 39% 70%)`
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <header></header>
+    <span className='partyTitle' >Theres a Party at 69 Pugsley</span>
+    <Map 
+      height={1000}
+    
+      center={center} 
+      zoom={zoom} 
+      onBoundsChanged={({ center, zoom }) => { 
+        setCenter(center) 
+        setZoom(zoom) 
+      }} 
+    >
+        <Marker 
+        width={50}
+        anchor={[45.92698182886522, -66.62735907832183]} 
+        color={color} 
+        onClick={() => setHue(hue + 20)} 
+      />
+      </Map>
     </div>
-  );
+  )
 }
-
 export default App;
