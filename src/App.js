2bfa0553b539a2fe56314c609c4783e498e67f7c
Tmaclea6 <tmaclea6@unb.ca>
2022-07-06 10:29:25 -0300

    adding multiple party points

--- src/App.js
+++ src/App.js
@@ -3,17 +3,32 @@ import './App.css';
 import React,{ useEffect, useState, useRef} from "react";
 import { Map, Marker } from 'pigeon-maps'
 import Box from '@mui/material/Box';
+import HostParty from './HostParty';
 
 function App() {
-  const [center, setCenter] = useState([45.92698182886522, -66.62735907832183])
-  const [zoom, setZoom] = useState(11)
-  const [hue, setHue] = useState(0)
-  const color = `hsl(${hue % 360}deg 39% 70%)`
+  const [center, setCenter] = useState([45.92698182886522, -66.62735907832183]);
+  const [zoom, setZoom] = useState(11);
+  const [hue, setHue] = useState(0);
+  const [markers, setMarkers] = useState(1);
+  const [markerLocations, setMarkerLocations] = useState(
+    {
+      data:[[45.92698182886522, -66.62735907832183],]
+    }
+  )
+  function addMarker(lat, lng) {
+    let hold = markerLocations;
+    hold.data[hold.data.length] = [lat, lng] ;
+    setMarkerLocations(hold);
+    setMarkers(markers+1);
+    console.log(markerLocations.data);
+  }
+  const color = `hsl(${hue % 360}deg 39% 70%)`;
   return (
     <div> 
-      <span class="partyTitle">Theres a Party at 69 Pugsley</span>
+      <span className="partyTitle">Theres a Party at 69 Pugsley</span>
       <div className ='centered'>
       <header></header>
+      <HostParty addMarker={addMarker}></HostParty>
       
       <Box className="rcorners1"
         sx={{
@@ -38,6 +53,10 @@ function App() {
           color={color} 
           onClick={() => setHue(hue + 20)} 
         />
+         {[...Array(markers).keys()].map((markers, i) => {
+      return <Marker key={i} width={50} anchor={markerLocations.data[i]} color={color} 
+      onClick={() => setHue(hue + 20)} />;
+    })}
         </Map>
         </Box>
       </div>
--- src/HostParty.js
+++ src/HostParty.js
@@ -3,6 +3,7 @@ import './App.css';
 import React,{ useEffect, useState, useRef} from "react";
 import { Map, Marker } from 'pigeon-maps'
 import Box from '@mui/material/Box';
+import Geocode from "react-geocode";
 function addressToGeo(address) {
     Geocode.fromAddress(address).then(
       (response) => {
@@ -16,29 +17,43 @@ function addressToGeo(address) {
     );
   }
 
-const HostParty = () => {
+const HostParty = ({addMarker}) => {
     const [openPartyModel, setOpenPartyModel] = useState(false);
-    let textInput = "EnterAddress";
+    let lng;
+    let lat;
+    function handleLng(event) {
+        lng = parseFloat(event.target.value);
+      }
+    function handleLat(event) {
+      lat = parseFloat(event.target.value);
+    }
+    function setOpen(){
+        setOpenPartyModel(true);
+    }
     function submit(){
-        const coords = addressToGeo(textInput);
-
+        //const coords = addressToGeo(textInput);
+        //if(lng!==undefined&&lat!==undefined){}
+        addMarker(lat,lng)
     }
     return(
+        
         <div>
-            {!openPartyModel && (
-                <button onClick={setOpenPartyModel(true)}>Host a party</button>
-            )};
+            
+                <button className='partyTitle' onClick={setOpen}>Host a party</button>
+           
             {openPartyModel && (
                 <div className='partyModel'>
                    <div className='modelContent'>
                        <span> 
-                        <textarea value={TextInput}> </textarea>
+                        <textarea onChange={handleLat} placeholder="Enter Latitude"></textarea>
+                        <textarea onChange={handleLng} placeholder="Enter Longitude"></textarea>
                         <button onClick={submit}>Enter</button>
                         </span>
                    </div>
                 </div>
-            )};
+            )}
         </div>
+        
     );
 };
 
